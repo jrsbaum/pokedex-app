f2da989daaa02543bbbaa542da612c0435bb36c1
Júnior Baum <juniorsbaum@hotmail.com>
2022-08-25 23:26:07 -0300

    feat: pokemon list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemoncardComponent } from './components/pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PokemonListComponent,
+    PokemoncardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent {
+  
+  pokemons = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander']
+
+}
