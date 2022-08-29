7296a24d319dbf79077446c3caf25a817ce8845c
Júnior Baum <juniorsbaum@hotmail.com>
2022-08-29 18:21:06 -0300

    feat: add service and consuming the API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemoncardComponent } from './components/pokemon-card/pokemon-card.component';
+import { HttpClientModule} from '@angular/common/http';
+import { PokemonService } from './services/pokemon.service';
 
 @NgModule({
   declarations: [
@@ -19,10 +21,11 @@ import { PokemoncardComponent } from './components/pokemon-card/pokemon-card.com
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     NgbModule
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
