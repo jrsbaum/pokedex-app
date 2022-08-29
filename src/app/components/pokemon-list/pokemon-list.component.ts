e0d33240eda7f1535111444827e830a65ccef37e
Júnior Baum <juniorsbaum@hotmail.com>
2022-08-29 18:19:44 -0300

    style: removes the CSS.

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,12 +1,28 @@
-import { Component} from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit} from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
-})
-export class PokemonListComponent {
-  
-  pokemons = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander']
+  })
+export class PokemonListComponent implements OnInit{
+  pokemons: any[] = [];
 
+  constructor (
+    public pokemonService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokemonService.getPokemons()
+      .subscribe((response: any) => {
+        response.results.forEach((result: { name: string; }) => {
+          this.pokemonService.getDetails(result.name)
+            .subscribe((uniqResponse: any) => {
+              this.pokemons.push(uniqResponse);
+              console.log(this.pokemons);
+            });
+        })
+      });
+  }
 }
