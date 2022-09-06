bf3804e65a57dd9a5cef24993f8d55d110211bdc
Júnior Baum <juniorsbaum@hotmail.com>
2022-09-06 13:18:40 -0300

    style: card color = pokemon color; secondary type

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -41,4 +41,81 @@ export class PokemonListComponent implements OnInit {
 
     return s;
   }
+  getColors(tipo: any) {
+    let x = '';
+
+    if (tipo == 'grass') {
+      x = '#d2f2c2';
+    }
+
+    if (tipo == 'poison') {
+      x = '#f7cdf7';
+    }
+
+    if (tipo == 'fire') {
+      x = '#ffd1b5';
+    }
+
+    if (tipo == 'flying:') {
+      x = '#eae3ff';
+    }
+
+    if (tipo == 'water') {
+      x = '#c2f3ff';
+    }
+
+    if (tipo == 'normal') {
+      x = '#e6e6c3';
+    }
+
+    if (tipo == 'electric') {
+      x = '#fff1ba';
+    }
+
+    if (tipo == 'ground') {
+      x = '#e0ccb1';
+    }
+
+    if (tipo == 'fighting') {
+      x = '#fcada9';
+    }
+
+    if (tipo == 'psychic') {
+      x = '#ffc9da';
+    }
+
+    if (tipo == 'rock') {
+      x = '#f0e09c';
+    }
+
+    if (tipo == 'fairy') {
+      x = '#ffdee5';
+    }
+
+    if (tipo == 'steel') {
+      x = '#e6eaf0';
+    }
+
+    if (tipo == 'ice') {
+      x = '#e8feff';
+    }
+
+    if (tipo == 'steel') {
+      x = '#e6eaf0';
+    }
+
+    if (tipo == 'ghost') {
+      x = '#dbbaff';
+    }
+
+    if (tipo == 'dragon') {
+      x = '#c4bdff';
+    }
+
+    if (tipo == 'dark') {
+      x = '#a9abb0';
+    }
+
+    return x;
+  }
 }
